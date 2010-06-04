2b7c8823ca59a2fd1b67de1256e4377a684c323b
Alex York <alex-york@hotmail.co.uk>
2010-06-04 17:58:37 +0200

    FizzBuzz kata completed by Alex and Nico

---
+++ FizzBuzz/FizzBuzzGame.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzGame
+    {
+        public string GetResult(int number)
+        {
+            if (number.IsDivisibleByFive() && number.IsDivisibleByThree())
+            {
+                return "FizzBuzz";
+            }
+
+            if (number.IsDivisibleByFive())
+            {
+                return "Buzz";
+            }
+            
+            if (number.IsDivisibleByThree())
+            {
+                return "Fizz";
+            }
+
+            return number.ToString();
+        }
+    }
+
+    public static class DivisionExtensions
+    {
+        public static bool IsDivisibleByFive(this int number)
+        {
+            return number % 5 == 0;
+        }
+
+        public static bool IsDivisibleByThree(this int number)
+        {
+            return number % 3 == 0;
+        }
+    }
+}
--- FizzBuzz/Tests/FizzBuzzTests.cs
+++ FizzBuzz/Tests/FizzBuzzTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
@@ -9,7 +8,52 @@ namespace FizzBuzz.Tests
     [TestFixture]
     public class FizzBuzzTests
     {
+        private FizzBuzzGame game;
 
+        [SetUp]
+        public void SetUp()
+        {
+            game = new FizzBuzzGame();
+        }
 
+        [Test]
+        public void Should_print_Fizz_when_number_is_divisible_by_3()
+        {
+            var result = game.GetResult(3);
+
+            result.ShouldBe("Fizz");
+        }
+
+        [Test]
+        public void Should_print_Buzz_when_number_is_divisible_by_5()
+        {
+            var result = game.GetResult(5);
+
+            result.ShouldBe("Buzz");
+        }
+
+        [Test]
+        public void Should_print_FizzBuzz_when_number_is_divisible_by_3_and_5()
+        {
+            var result = game.GetResult(15);
+
+            result.ShouldBe("FizzBuzz");
+        }
+
+        [Test]
+        public void Should_print_the_number_when_it_is_not_divisible_by_3_or_5()
+        {
+            var result = game.GetResult(1);
+
+            result.ShouldBe("1");
+        }
+    }
+
+    public static class TestExtensions
+    {
+        public static void ShouldBe(this object actual, object expected)
+        {
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
